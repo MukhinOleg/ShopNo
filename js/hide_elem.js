00b55313793d2b38c3b13246952f4df875b2f3f6
MukhinOleg <oleg-mukhin-82@mail.ru>
2017-08-12 12:06:41 +0300

    Update hide_elem.js

--- js/hide_elem.js
+++ js/hide_elem.js
@@ -1,5 +1,5 @@
 
-document.querySelector('.positioneer').addEventListener('click', function press(event){
+document.querySelector('.set_bg').addEventListener('click', function press(event){
   var target = event.target;
   document.querySelector('.under_bground').classList.toggle('hide');
   
