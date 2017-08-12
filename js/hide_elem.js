c0b7c152e70d93d405e9d15131df3def30cdbd6a
MukhinOleg <oleg-mukhin-82@mail.ru>
2017-08-12 11:48:51 +0300

    Add files via upload

---
+++ js/hide_elem.js
@@ -0,0 +1,6 @@
+
+document.querySelector('.positioneer').addEventListener('click', function press(event){
+  var target = event.target;
+  document.querySelector('.under_bground').classList.toggle('hide');
+  
+})
