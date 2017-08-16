75a0ad7143d0e114e8d4adb86f9ead608dd64c50
MukhinOleg <oleg-mukhin-82@mail.ru>
2017-08-17 00:14:11 +0300

    Delete nav_menu.js

--- js/nav_menu.js
+++
@@ -1,8 +0,0 @@
-
-
-document.querySelector('.nav_menu_container').addEventListener('click', function press(event){
-  var target = event.target;
-  document.querySelector('.header__menu_hover').classList.toggle('hide');
-  document.querySelector('.header__menu').classList.toggle('hide');
-  
-})
