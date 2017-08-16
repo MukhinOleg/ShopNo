dce8387aa5a0bbd4efc0a4008518a68a4372d6d6
MukhinOleg <oleg-mukhin-82@mail.ru>
2017-08-17 00:14:02 +0300

    Delete hide_elem.js

--- js/hide_elem.js
+++
@@ -1,6 +0,0 @@
-
-document.querySelector('.set_bg').addEventListener('click', function press(event){
-  var target = event.target;
-  document.querySelector('.under_bground').classList.toggle('hide');
-  
-})
